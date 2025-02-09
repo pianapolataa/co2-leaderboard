f7e846afee4ac33536156c795ebd5f29b057d082
= <sissi.hu@hotmail.com>
2025-02-08 21:41:03 -0500

    update neural network prediction code

--- co2pred.py
+++ co2pred.py
@@ -14,17 +14,18 @@ print(f"Using {device} device")
 def calculate_mae(predictions, targets):
     return torch.mean(torch.abs(predictions - targets))
 
+maxi = 681
+mini = 2.4
 
 class TextDataset():
     def __init__(self, file_path):
-        self.data = pd.read_excel(file_path)
+        self.data = pd.read_excel(file_path, header = None)
         self.data = self.data.values  # Convert DataFrame to NumPy array before using PyTorch
         self.data = torch.tensor(self.data, dtype=torch.float32)
+        self.data = (self.data - self.data.min()) / (self.data.max() - self.data.min())
         
         self.inputs = self.data[:, :-5]
-        self.outputs = self.data[:, 1:]
-        self.inputs = 2 * (self.inputs - self.inputs.min()) / (self.inputs.max() - self.inputs.min()) - 1
-        self.outputs = 2 * (self.outputs - self.outputs.min()) / (self.outputs.max() - self.outputs.min()) - 1
+        self.outputs = self.data[:, -1:]
 
     def __len__(self):
         return len(self.data)
@@ -75,12 +76,13 @@ def train(dataloader, model, loss_fn, optimizer):
         loss, current = loss.item(), (batch + 1) * len(X)
         # print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
-def test(dataloader, model, loss_fn):
+def test(dataloader, model, loss_fn, record):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.eval()
     test_loss =  0
     total_mae = 0  # Variable to accumulate MAE
+    all_preds = []
     with torch.no_grad():
         for X, y in dataloader:
             X, y = X.to(device), y.to(device)
@@ -89,48 +91,51 @@ def test(dataloader, model, loss_fn):
 
             mae = calculate_mae(pred, y)
             total_mae += mae.item()
+
+            all_preds.append(pred.cpu().numpy().flatten())
+
     test_loss /= num_batches
     total_mae /= num_batches 
 
     print(f"Test Error: \n Avg loss: {test_loss:.4f} \n Avg MAE: {total_mae:.4f}")
-    # return pred.cpu().numpy()
+    if (record == True):
+        all_preds = np.concatenate(all_preds, axis=0)  
+        all_preds = [x * (maxi - mini) + mini for x in all_preds]
+        all_preds = np.array(all_preds, dtype = np.float32)
+        return all_preds
 
-# def gety(dataloader):
-#     with torch.no_grad():
-#         for X, y in dataloader:
-#             y = y.to(device)
-#     return y.cpu().numpy()
+def gety(dataloader):
+    with torch.no_grad():
+        for X, y in dataloader:
+            y = y.to(device)
+    return y.cpu().numpy()
 
 
 
 if __name__ == "__main__":
-
-    # training_data = TextDataset("/Users/sissi/Downloads/pythonstuff/anyskin/anyskin/visualizations/data_2025-02-03_20-39-21.txt")
-    # test_data = TextDataset("/Users/sissi/Downloads/pythonstuff/anyskin/anyskin/visualizations/contdata_2025-02-06_11-55-46.txt")
-
-    
     training_data = TextDataset('/Users/sissi/Downloads/table1_train.xlsx')
     test_data = TextDataset('/Users/sissi/Downloads/table1_test.xlsx')
+    pred_data = TextDataset('/Users/sissi/Downloads/table1 copy.xlsx')
 
-    train_dataloader = DataLoader(training_data, batch_size=10)
-    test_dataloader = DataLoader(test_data, batch_size=10)
+
+    train_dataloader = DataLoader(training_data, batch_size=10, drop_last = False)
+    test_dataloader = DataLoader(test_data, batch_size=10, drop_last = False)
+    pred_dataloader = DataLoader(pred_data, batch_size=51, drop_last=False)
 
     epochs = 1000
     for t in range(epochs):
         train(train_dataloader, model, loss_fn, optimizer)
-        if (t % 50 == 0): test(test_dataloader, model, loss_fn)
+        if (t % 50 == 0): test(test_dataloader, model, loss_fn, False)
 
-   
-    # res = test(test_dataloader, model, loss_fn).T
-    # act = gety(test_dataloader).T
-    # t = np.zeros(len(res[0]))
-    # for i in range(len(res[0])):
-    #     t[i] = i
-    # plt.plot(t, res[50], label = "predicted", linestyle="-.")
-    # plt.plot(t, act[50], label = "actual", linestyle="-")
-    # plt.legend()
-    # plt.show()
+    
+    pred = test(pred_dataloader, model, loss_fn, True)
+    print(pred)
 
+
+# Convert NumPy array to csv
+    pred = pd.DataFrame(pred)
+    pred.to_csv('output.csv', index=False, header=False)
+    
 
 
     print("Done!")
